a37d401fdc6147467bf6db4f04b226e082dfeea6
Seng Keat <corodostudio@gmail.com>
2023-09-01 16:40:34 +0700

    Jjcj168xmL@90.,?

--- 2. Advanced Linear Algebra.sh
+++ 2. Advanced Linear Algebra.sh
@@ -108,6 +108,143 @@ unitary similarity
 unitary triangularization
 normal matrices and unitary diagonalization
 polynormials and commuting matrices
+simultaneous unitary diagonalization
+polar decomposition invertible case
+polar decomposition general case
+interlacing eigenvalues for hermitian matrices
+determinant criterion for position definite matrices
+
+jordan canonical form
+nilpotent maps
+partition diagrams
+computing image via partition diagrams
+computing null spaces via partition diagrams
+classification of nilpotent maps
+fitting lemma
+uniqueness of jordan canonical forms
+computing jordan canonocal forms
+application to differential equations
+minimal polynomial
+jordan chevalley decomposition of a linear operator
+
+matrix factorization
+approximation by orthonormal vectors
+gram schmidt orthonormalization
+gram schmidt qr factorization
+householder reflection
+householder qr factorization
+LU factorization
+gaussian elimination
+permuted LU factorizations
+cholesky factorization
+least squares approximation
+singular value decomposition
+
+iterative algorithm in numerical linear algebra
+richardson algorithm
+jacobi algorithm
+gauss seidel algorithm
+vector norms
+metric spaces
+convergence of sequences
+comparable norms
+matrix norms
+formulas for matrix norms
+matrix inversion via geometric series
+affine iteration and richardson algorithm
+splitting matrices and jacobi algorithm
+induced matrix norm and spectral radious
+analysis of the gauss seidel algorithm
+shifted and inverse power method
+deflation
+
+interplay of geometry and linear algebra
+affine geometry and convexity
+linear subspaces
+example of linear subspaces
+affine combination and affine sets
+affine sets and linear subspaces
+affine span of a set
+affine independence
+affine bases and barycentric coordinates
+characterization of affine sets
+affine maps
+convex sets
+convex hullls
+caratheodory theorem
+hyperplanes and half space in Rn
+closed convex sets
+cones and convex cones
+intersection lemma for V cones
+all h cones are v cones
+finite intersections of closed half space
+convex functions
+derivative test for convex functions
+
+ruler and compass constructions
+geometry constructibility
+arithmetic constructibility
+preliminaries on field extensions
+field theoretic constructibility
+proof that GC belong to AC
+proof that AC belong to GC
+algebraic elements and minimal polynomials
+proof of AC = SQC
+impossibility of the 17 Gon
+solvabilities by radicals
+
+dual spaces and bilinear forms
+vector spaces of linear maps
+dual bases
+zero sets
+annihilators
+double dual V**
+correspondence between subspace of V and V*
+dual maps
+nondegenerate bilinear forms
+real inner product spaces
+complex inner product spaces
+comment on infinite dimensional spaces
+affine algebraic geometry
+
+metrics spaces and hilbert spaces
+convergent sequences
+closed sets
+open sets
+continuous functions
+compact set
+completeness
+proof of the hibert spaces axiom for l2(x)
+basic properties of hilbert spaces
+orthogonal complements
+orthogonal sets
+maximal orthogonal sets
+isomorphism of H and L2(x)
+isomorphism of H and L2(X)
+continuous linear maps
+dual space of a hilbert space
+adjoint
+
+modules independence and classification theorem
+finitely generated commutative groups
+commutative groups
+generating sets
+Z independence and Z bases
+elementary operation on Z bases
+UMP for free communitative groups
+coordinates and Z linear maps
+elementary operations and changes of basic
+reduction theorem for integer matrices
+structure of z linear maps between free groups
+structure of finitely generated commitative groups
+example of reduction algorithm
+special subgroups
+uniqueness proof : free case
+prime power cases
+elementary divisors
+invariant factors
+uniqueness proof: general cases
+
 
 
 
